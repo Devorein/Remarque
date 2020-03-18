b935fea988d1fb5285150ef961e94697e4ea9841
Devon Reid <fireborn1003@gmail.com>
2020-03-18 18:16:09 +0600

    F: Added option to pass output dir
    User can now pass custom output directory to specify where to put the output .html files
    using the --output option which has a default value of OUTPUT

--- index.js
+++ index.js
@@ -6,28 +6,27 @@ const argv = require('yargs').argv;
 const { convert } = require('./lib/convert');
 const { showSuccessMessage, showErrorMessage } = require('./lib/output');
 
-const currentDir = process.cwd();
+const CURRENT_DIR = process.cwd();
 
 const chapters = {};
 
-fs.readdirSync(currentDir).filter((chapter) => chapter.match(/^[0-9]{1,2}\./)).forEach((chapter) => {
+fs.readdirSync(CURRENT_DIR).filter((chapter) => chapter.match(/^[0-9]{1,2}\./)).forEach((chapter) => {
 	chapters[chapter.match(/^[0-9]{1,2}/)[0]] = chapter;
 });
 
 if (argv.convert) {
+	let output_dir = null;
+	if (argv.output) {
+		output_dir = path.join(CURRENT_DIR, typeof argv.output === 'string' ? argv.output : 'OUTPUT');
+		if (!fs.existsSync(output_dir)) fs.mkdirSync(output_dir);
+	}
 	const chapter_to_convert = argv.convert;
 	if (Number.isInteger(chapter_to_convert) && chapter_to_convert > 0) {
 		const chapter_name = chapters[chapter_to_convert];
-		if (chapter_name) {
-			const chapter_dir = path.join(currentDir, chapter_name);
-			// convert(chapter_dir, argv.output);
-		}
+		if (chapter_name) convert(path.join(CURRENT_DIR, chapter_name), output_dir);
 		else showErrorMessage(`chapter doesn't exist`);
 	}
 	else if (typeof chapter_to_convert === 'boolean') {
-		chapters.forEach((chapter) => {
-			const chapter_dir = path.join(currentDir, chapter);
-			convert(chapter_dir);
-		});
+		for (let chapter in chapters) convert(path.join(CURRENT_DIR, chapters[chapter]), output_dir);
 	}
 }
