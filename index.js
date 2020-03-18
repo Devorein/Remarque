c1189da7992ed9ec98963fe86d94c7e2f9137035
Devon Reid <fireborn1003@gmail.com>
2020-03-18 17:50:19 +0600

    I: Chapter number as --convert arg
    User can now just pass the chapter number to indicate which chapter to convert.

--- index.js
+++ index.js
@@ -4,23 +4,27 @@ const fs = require('fs');
 const path = require('path');
 const argv = require('yargs').argv;
 const { convert } = require('./lib/convert');
-const { showSuccessMessage } = require('./lib/output');
+const { showSuccessMessage, showErrorMessage } = require('./lib/output');
 
 const currentDir = process.cwd();
 
-const chapters = fs.readdirSync(currentDir).filter((chapter) => chapter.match(/^[0-9]{1,2}\./)).sort((a, b) => {
-	let prev = parseInt(a.substring(0, a.indexOf('.')));
-	let next = parseInt(b.substring(0, b.indexOf('.')));
-	return prev - next;
+const chapters = {};
+
+fs.readdirSync(currentDir).filter((chapter) => chapter.match(/^[0-9]{1,2}\./)).forEach((chapter) => {
+	chapters[chapter.match(/^[0-9]{1,2}/)[0]] = chapter;
 });
 
 if (argv.convert) {
-	const chapter_num = argv.convert;
-	if (Number.isInteger(chapter_num) && chapter_num > 0) {
-		const chapter_dir = path.join(currentDir, chapters[chapter_num - 1]);
-		convert(chapter_dir);
+	const chapter_to_convert = argv.convert;
+	if (Number.isInteger(chapter_to_convert) && chapter_to_convert > 0) {
+		const chapter_name = chapters[chapter_to_convert];
+		if (chapter_name) {
+			const chapter_dir = path.join(currentDir, chapter_name);
+			// convert(chapter_dir, argv.output);
+		}
+		else showErrorMessage(`chapter doesn't exist`);
 	}
-	else {
+	else if (typeof chapter_to_convert === 'boolean') {
 		chapters.forEach((chapter) => {
 			const chapter_dir = path.join(currentDir, chapter);
 			convert(chapter_dir);
