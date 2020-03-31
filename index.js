aee98c81e699b1648b150c326149ba920d38ec8a
Devon Reid <fireborn1003@gmail.com>
2020-03-31 14:59:39 +0600

    F: Overlay counter, arrow, cancel

--- index.js
+++ index.js
@@ -3,10 +3,12 @@
 const fs = require('fs');
 const path = require('path');
 const argv = require('yargs').argv;
+
 const { convert } = require('./lib/convert');
 const { showSuccessMessage, showErrorMessage } = require('./lib/output');
+const { makeConstantProp } = require('./lib/utility');
 
-const CURRENT_DIR = process.cwd();
+makeConstantProp('CURRENT_DIR', process.cwd());
 
 const chapters = {};
 
@@ -15,18 +17,28 @@ fs.readdirSync(CURRENT_DIR).filter((chapter) => chapter.match(/^[0-9]{1,2}\./)).
 });
 
 if (argv.convert) {
-	let output_dir = null;
 	if (argv.output) {
-		output_dir = path.join(CURRENT_DIR, typeof argv.output === 'string' ? argv.output : 'OUTPUT');
-		if (!fs.existsSync(output_dir)) fs.mkdirSync(output_dir);
+		makeConstantProp(
+			'OUTPUT_DIR',
+			path.join(CURRENT_DIR, typeof argv.output === 'string' ? argv.output : 'OUTPUT')
+		);
+		if (!fs.existsSync(OUTPUT_DIR)) fs.mkdirSync(OUTPUT_DIR);
 	}
+	else makeConstantProp('OUTPUT_DIR', null);
 	const chapter_to_convert = argv.convert;
 	if (Number.isInteger(chapter_to_convert) && chapter_to_convert > 0) {
 		const chapter_name = chapters[chapter_to_convert];
-		if (chapter_name) convert(path.join(CURRENT_DIR, chapter_name), output_dir);
-		else showErrorMessage(`chapter doesn't exist`);
+		if (chapter_name) {
+			makeConstantProp('CHAPTER_NAME', chapter_name);
+			makeConstantProp('FILE_DIR', path.join(CURRENT_DIR, chapter_name));
+			convert();
+		}
+		else showErrorMessage(`chapter ${chapter_to_convert} doesn't exist`);
 	}
-	else if (typeof chapter_to_convert === 'boolean') {
-		for (let chapter in chapters) convert(path.join(CURRENT_DIR, chapters[chapter]), output_dir);
-	}
+	else if (typeof chapter_to_convert === 'boolean')
+		for (let chapter in chapters) {
+			makeConstantProp('FILE_DIR', path.join(CURRENT_DIR, chapters[chapter]));
+			makeConstantProp('CHAPTER_NAME', chapters[chapter]);
+			convert();
+		}
 }
